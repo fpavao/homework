8930a48fb53c676c7fef328ca10518ae3ad2dfd0
Fabio Pavao <fabio.pavao@gmail.com>
2011-12-25 22:15:44 -0200

    Exemplo de lazy singleton.

---
+++ src/LazySingleton.java
@@ -0,0 +1,20 @@
+/**
+ * @author Fabio Pavao
+ * @since 25/12/11
+ */
+public class LazySingleton {
+
+    private static LazySingleton instance = null;
+
+    private LazySingleton() {
+    }
+
+    public static synchronized LazySingleton getInstance() {
+        if (instance == null)
+            instance = new LazySingleton();
+
+        return  instance;
+    }
+
+
+}
